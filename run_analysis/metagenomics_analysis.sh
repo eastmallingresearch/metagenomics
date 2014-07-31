d8d4cc4089a9b02e9d4888f66ecb562a6e9885d3
Richard Harrison <richard.harrison@emr.ac.uk>
2014-07-31 18:27:09 +0100

    Path error

--- run_analysis/metagenomics_analysis.sh
+++ run_analysis/metagenomics_analysis.sh
@@ -18,7 +18,7 @@ cd $line
 echo "Working directory "
 echo  `pwd`
 pick_de_novo_otus.py -i seqs.fna -o pick_otus.out -v
-assign_taxonomy.py  -i pick_otus.out/rep_set/seqs_rep_set.fasta -m rtax --single_ok --amplicon_id_regex='(\S+)\s+(\S+)' --header_id_regex='\S+\s+(\S+)'   --read_1_seqs_fp seqs.fna --read_2_seqs_fp ../$line$R2/seqs.fna -r  $SQPATH  -t  $TXPATH  -v
+assign_taxonomy.py  -i pick_otus.out/rep_set/seqs_rep_set.fasta -m rtax --single_ok --amplicon_id_regex='(\S+)\s+(\S+)' --header_id_regex='\S+\s+(\S+)'   --read_1_seqs_fp seqs.fna --read_2_seqs_fp ../$line$R2/seqs.fna -r  ../$SQPATH  -t  ../$TXPATH  -v
 cd ..
 
 else
