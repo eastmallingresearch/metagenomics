71ddf7c0c4b0b057e3e2cc03c7f45706bf35acee
Richard Harrison <richard.harrison@emr.ac.uk>
2014-07-31 13:10:19 +0100

    analysis

--- run_analysis/metagenomics_analysis.sh
+++ run_analysis/metagenomics_analysis.sh
@@ -15,7 +15,7 @@ if [ "$last" = "R1" ]
 then
    echo "Processing file R1"
 cd $line   
-pick_de_novo_otus.py -i seqs.fna -o pick_otus.out -v
+#pick_de_novo_otus.py -i seqs.fna -o pick_otus.out -v
 assign_taxonomy.py  -i pick_otus.out/rep_set/seqs_rep_set.fasta -m rtax --single_ok --amplicon_id_regex='(\S+)\s+(\S+)' --header_id_regex='\S+\s+(\S+)'   --read_1_seqs_fp seqs.fna --read_2_seqs_fp ../$line$R2/seqs.fna -r  $SQPATH  -t  $TXPATH  -v
 cd ..
 
