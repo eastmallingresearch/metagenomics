34dc0e2e0e7093105641f806d28313e306d80984
Richard Harrison <richard.harrison@emr.ac.uk>
2014-07-31 13:07:45 +0100

    Removing hardcoding

--- run_analysis/metagenomics_analysis.sh
+++ run_analysis/metagenomics_analysis.sh
@@ -16,7 +16,7 @@ then
    echo "Processing file R1"
 cd $line   
 pick_de_novo_otus.py -i seqs.fna -o pick_otus.out -v
-assign_taxonomy.py  -i pick_otus.out/rep_set/seqs_rep_set.fasta -m rtax --single_ok --amplicon_id_regex='(\S+)\s+(\S+)' --header_id_regex='\S+\s+(\S+)'   --read_1_seqs_fp seqs.fna --read_2_seqs_fp $HOME/projects/xiangming_metagenomics/feb_2014/$line$R2/seqs.fna -r  $SQPATH  -t  $TXPATH  -v
+assign_taxonomy.py  -i pick_otus.out/rep_set/seqs_rep_set.fasta -m rtax --single_ok --amplicon_id_regex='(\S+)\s+(\S+)' --header_id_regex='\S+\s+(\S+)'   --read_1_seqs_fp seqs.fna --read_2_seqs_fp ../$line$R2/seqs.fna -r  $SQPATH  -t  $TXPATH  -v
 cd ..
 
 else
